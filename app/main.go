d0c06a586fe65aa0d2fcf2229f41a1936e2fbf9a
yoshi_iv_44 <yoshtiaka.i.v.x.jade@gmail.com>
2022-02-25 20:04:22 +0900

    check heroku deploy

--- app/main.go
+++ app/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"github.com/44taka/golang-gin/infrastructure"
-	"github.com/44taka/golang-gin/infrastructure/persistence"
-	"github.com/44taka/golang-gin/interfaces/handler"
-	"github.com/44taka/golang-gin/usecase"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	// コンフィグ読み込みaa
-	config := infrastructure.NewConfig()
-	db := infrastructure.NewDB(config)
+	// config := infrastructure.NewConfig()
+	// db := infrastructure.NewDB(config)
 
-	userPersistence := persistence.NewUserPersistence(db.Connect())
-	userUseCase := usecase.NewUserUseCase(userPersistence)
-	userHandler := handler.NewUserHandler(userUseCase)
+	// userPersistence := persistence.NewUserPersistence(db.Connect())
+	// userUseCase := usecase.NewUserUseCase(userPersistence)
+	// userHandler := handler.NewUserHandler(userUseCase)
 
 	r := gin.Default()
-	r.GET("/users", func(ctx *gin.Context) { userHandler.FindAll(ctx) })
-	r.GET("/users/:id", func(ctx *gin.Context) { userHandler.FindById(ctx) })
-	r.POST("/users", func(ctx *gin.Context) { userHandler.Create(ctx) })
-	r.PUT("/users/:id", func(ctx *gin.Context) { userHandler.Update(ctx) })
-	r.DELETE("/users/:id", func(ctx *gin.Context) { userHandler.Delete(ctx) })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	// r.GET("/users", func(ctx *gin.Context) { userHandler.FindAll(ctx) })
+	// r.GET("/users/:id", func(ctx *gin.Context) { userHandler.FindById(ctx) })
+	// r.POST("/users", func(ctx *gin.Context) { userHandler.Create(ctx) })
+	// r.PUT("/users/:id", func(ctx *gin.Context) { userHandler.Update(ctx) })
+	// r.DELETE("/users/:id", func(ctx *gin.Context) { userHandler.Delete(ctx) })
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
